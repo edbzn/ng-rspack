287e3ab0d9560c8533b8265391cc5c629cc7241e
Edouard Bozon <bozonedouard@gmail.com>
2024-07-10 14:38:08 +0200

    chore: fix top level async/await issue

--- apps/mfe3/esbuild-mf-plugin.ts
+++ apps/mfe3/esbuild-mf-plugin.ts
@@ -4,9 +4,10 @@ const { withFederation } = require('@module-federation/esbuild/build');
 export default moduleFederationPlugin(
   withFederation({
     name: 'mfe3',
+
     exposes: {
       './app': './apps/mfe3/src/app/app.component.ts',
     },
-    shared: ['@angular/core', '@angular/common', '@angular/router'],
+    shared: [],
   })
 );
